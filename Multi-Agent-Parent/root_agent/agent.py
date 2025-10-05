75f7160422f2b4b15fc3edf945e74af2662193d1
Mahesh <maheshddivate@gmail.com>
2025-10-06 02:40:49 +0530

    init: Document Demistifier

--- Multi-Agent-Parent/root_agent/agent.py
+++ Multi-Agent-Parent/root_agent/agent.py
@@ -109,9 +109,9 @@ try:
         model = MODEL_GEMINI_2_0_FLASH,
         # model=LiteLlm(model=MODEL_GPT_4O), # If you would like to experiment with other models
         name="farewell_agent",
-        instruction="You are the Farewell Agent. Your ONLY task is to provide a polite goodbye message. "
-                    "Use the 'say_goodbye' tool when the user indicates they are leaving or ending the conversation "
-                    "(e.g., using words like 'bye', 'goodbye', 'thanks bye', 'see you'). "
+        instruction="You are the Farewell Agent. Your ONLY task is to provide a polite goodbye message."
+                    "Use the 'say_goodbye' tool when the user indicates they are leaving or ending the conversation"
+                    "(e.g., using words like 'bye', 'goodbye', 'thanks bye', 'see you')."
                     "Do not perform any other actions.",
         description="Handles simple farewells and goodbyes using the 'say_goodbye' tool.", # Crucial for delegation
         tools=[say_goodbye],
@@ -132,13 +132,15 @@ root_agent = Agent(
                 "You have specialized sub-agents: "
                 "1. 'greeting_agent': Handles simple greetings like 'Hi', 'Hello'. Delegate to it for these. "
                 "2. 'farewell_agent': Handles simple farewells like 'Bye', 'See you'. Delegate to it for these. "
-                "Analyze the user's query. If it's a greeting, delegate to 'greeting_agent'. If it's a farewell, delegate to 'farewell_agent'. "
+                "Analyze the user's query. If it's a greeting, delegate to 'greeting_agent'. If it's a farewell, delegate to 'farewell_agent'."
                 "If it's a weather request, handle it yourself using 'get_weather'. "
                 "For anything else, respond appropriately or state you cannot handle it.",
     tools=[get_weather], # Root agent still needs the weather tool for its core task
     # Key change: Link the sub-agents here!
     sub_agents=[greeting_agent, farewell_agent]
 )
+
+
 
 # Sample queries to test the agent: 
 
